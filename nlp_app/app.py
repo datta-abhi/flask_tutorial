d973d8f33466bcb49bb6c84de537a069588b60bf
datta-abhi <rookbee91@gmail.com>
2024-03-09 21:01:41 +0530

    message to render template for login, and perform_login function added

--- nlp_app/app.py
+++ nlp_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect
 from db import *
 
 app = Flask(__name__)
@@ -23,8 +23,25 @@ def perform_registration():
     response = dbo.insert(name,email,password)
 
     if response == 1:
-        return "Registration successful"
+        return render_template('login.html', message = "Registration successful. Login to proceed")
     else:
-        return "email already registered"
+        return render_template('register.html', message = "Email already exists")
+
+@app.route('/perform_login',methods = ['post'])
+def perform_login():
+    # check and then execute login actions if email and password matches
+    email = request.form.get('useremail')
+    password = request.form.get('password')
+
+    response = dbo.search(email,password)
+
+    if response == 1:
+        return 'Welcome'
+    else:
+        return "Email or password combination does not exist"
+
+@app.route('/profile')
+def profile():
+    return "Profile"
 
 app.run(debug=True)
