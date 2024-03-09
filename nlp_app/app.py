19a84c13c96529e6117d6df5e6393bcae63ebcdd
datta-abhi <rookbee91@gmail.com>
2024-03-09 13:29:59 +0530

    register and perform registration to save user data added

--- nlp_app/app.py
+++ nlp_app/app.py
@@ -6,4 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('login.html')
 
+# create registration page
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/perform_registration')
+def perform_registration():
+    return "Something"
+
 app.run(debug=True)
